499f7192fdf845296701897581cc614005fd5ac4
AlfredoRios24 <alfredoerios_03@hotmail.com>
2025-07-26 19:13:46 +0200

    modifcar_enlaces

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -6,3 +6,4 @@ export const API_URL = 'http://localhost:8091/api';
 export const API_URL_NOTES = 'http://localhost:8092/api/notes';
 
 export const API_URL_SECTIONS = 'http://localhost:8092/api/sections';
+
